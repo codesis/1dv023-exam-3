53a565833ed207ec9d4b3a1b925481ddc39a4d4b
emma <hassling.emma@gmail.com>
2019-04-10 21:59:15 +0200

    Small change in appjs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const helmet = require('helmet')
 const app = express()
 app.use(helmet())
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 app.engine('.hbs', hbs({
   defaultLayout: 'main',
