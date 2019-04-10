b2d147ecdfedb121a5dfa8866d0fc4b71e2e40e0
emma <hassling.emma@gmail.com>
2019-04-10 12:07:15 +0200

    Testing commit

--- app.js
+++ app.js
@@ -14,8 +14,5 @@ const fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000
 
-const server = https.createServer({
-  key: fs.readFileSync('./config/')
-})
 
 // git token: 5040f36ebe5609430a4156266c42ea2dc1ca6abe
