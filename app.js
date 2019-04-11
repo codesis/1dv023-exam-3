63a2eff2dd4ee3b8bbb1000372e5e92581a393e0
emma <hassling.emma@gmail.com>
2019-04-11 16:50:16 +0200

    Working on app module

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express()
 app.use(helmet())
 
 const https = require('https')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 app.engine('.hbs', hbs({
   defaultLayout: 'main',
