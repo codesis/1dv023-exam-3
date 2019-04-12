4393d7061340310a1c30cf63c557cd7e271cf5c2
emma <hassling.emma@gmail.com>
2019-04-12 11:25:39 +0200

    Changes in appjs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(helmet())
 
 const https = require('https')
 const hostname = 'localhost'
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.engine('.hbs', hbs({
   defaultLayout: 'main',
