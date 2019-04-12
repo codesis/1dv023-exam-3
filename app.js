0905ba45ab3a674f96c19a2655511d62a2e1d2b3
emma <hassling.emma@gmail.com>
2019-04-12 12:33:23 +0200

    Changes in appjs

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const GitHubWebhook = require('express-github-webhook')
 const app = express()
 app.use(helmet())
 
-const https = require('https')
-const hostname = 'localhost'
+const https = require('https').Server(app)
+const io = require('socket.io')(https)
 const port = process.env.PORT || 8080
 
 app.engine('.hbs', hbs({
@@ -36,8 +36,9 @@ app.use(bodyParser.json())
 // routes
 app.use('/', require('./routes/homeRouter.js'))
 
-// starting the server
-app.listen(port, '192.168.20.50', () => console.log('Server running on localhost' + port))
-
 // websocket server
-// const io = require('socket.io')(server)
+io.on('connection', function (socket) {
+  console.log('Socket running')
+})
+// starting the server '192.168.20.50'
+https.listen(port, () => console.log('Server running on:' + port))
