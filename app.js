c5bfa36a339ef69742961c6307fa75bbc30407d9
codesis <hassling.emma@gmail.com>
2019-07-23 11:27:27 +0200

    Merge

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.engine('.hbs', hbs({
 app.set('view engine', '.hbs')
 app.use(express.static(path.join(__dirname, '/public/')))
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json())
 
 // routes
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,11 +2,35 @@
 
 require('dotenv').config()
 const router = require('express').Router()
-// const rp = require('request-promise')
+const github = require('octonode')
+const client = github.client(process.env.GITHUBTOKEN)
+const repo = client.repo('1dv023/ek222re-examination-3')
 
 // Render index page with login form
 router.route('/').get(function (req, res) {
-  res.render('home/index')
+  repo.issues((err, data) => {
+    if (err) {
+      res.send(err)
+    }
+    const context = {
+      issues: data.map(function (issue) {
+        return {
+          id: issue.id,
+          number: issue.number,
+          user: issue.user.login,
+          title: issue.title,
+          body: issue.body,
+          link: issue.html_url,
+          created: issue.created,
+          updated: issue.updated,
+          commentsCount: issue.comments
+        }
+      })
+    }
+    res.render('home/index', {
+      issues: context.issues
+    })
+  })
 })
 
 module.exports = router
